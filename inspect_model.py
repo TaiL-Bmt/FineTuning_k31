1a86844036e1d76eec8fc56b6480ea9530815971
TaiL-Bmt <trongtaile1990@gmail.com>
2022-08-03 20:24:33 +0700

    update inspect_model argument parse

--- inspect_model.py
+++ inspect_model.py
@@ -2,11 +2,13 @@ from tensorflow.keras.applications import VGG16
 import argparse
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--include_top", type=int, default=1, help="Whether or not to include the top layers of model")
+ap.add_argument("-i", "--include_top", type=bool, default=True, help="Whether or not to include the top layers of model")
 args = vars(ap.parse_args())
 
+_include_top = args['include_top']
+
 print("[INFO] loading network...")
-model = VGG16(weights="imagenet", include_top = args["include_top"]>0)
+model = VGG16(weights="imagenet", include_top=_include_top)
 
 print("[INFO] showing layers...")
 for (i, layer) in enumerate(model.layers):
